ab8075e326fc2570eec5d9978b46107abce1468a
KumarSurajwebdeveloper <kr.surajkumar09@gmail.com>
2026-03-17 00:06:50 +0530

    Field Injection

--- src/main/java/org/interview/Service/PatientServiceImpl.java
+++ src/main/java/org/interview/Service/PatientServiceImpl.java
@@ -3,6 +3,7 @@ package org.interview.Service;
 
 import org.interview.Entity.Patient;
 import org.interview.Repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,12 +11,16 @@ import java.util.List;
 @Service
 public class PatientServiceImpl implements PatientService {
 
-    private final PatientRepository patientRepository;
+//    private final PatientRepository patientRepository;
 
     // Constructor Injection
-    public PatientServiceImpl(PatientRepository patientRepository) {
-        this.patientRepository = patientRepository;
-    }
+//    public PatientServiceImpl(PatientRepository patientRepository) {
+//        this.patientRepository = patientRepository;
+//    }
+
+    @Autowired
+    private PatientRepository patientRepository;
+
 
     @Override
     public Patient createPatient(Patient patient) {
